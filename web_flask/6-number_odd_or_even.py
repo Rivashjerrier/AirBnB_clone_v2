9a37090daf7d98782538d43aa34ad05dc828c3d3
Rivashjerrier <rivijerrier@gmail.com>
2023-01-18 13:01:45 +0000

    starts a Flask web application

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -54,5 +54,6 @@ def number_odd_or_even(n):
     else:
         return render_template('6-number_odd_or_even.html', n=n, result=int_odd)
 
+
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0")
