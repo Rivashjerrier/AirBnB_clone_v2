4aa6d8c973b3dceaca7b345effe943d3deb86fa4
Rivashjerrier <rivijerrier@gmail.com>
2023-01-18 10:30:34 +0000

    starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+""" starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Prints Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """ Prints C followed by the value of the text variable """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """ Prints Python, followed by the value of the text variable """
+    return "Python {:s}".format(text).replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ Prints n is a number, only if n is an integer """
+    return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
